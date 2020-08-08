653914a4eda28803e2b70281628a28ecb89e4586
Yash <yaswanth.iddibani@gmail.com>
2020-08-08 09:35:40 +0100

    SingleTon Pattern

---
+++ singletonPattern.js
@@ -0,0 +1,25 @@
+const singleTon = (function(){
+    let instance;
+
+    function createInstance(){
+        const object = new Object({name:"Yash"});
+        return object;
+    }
+    return {
+
+        getInstance: function(){
+            if(!instance)
+                instance = createInstance();
+            return instance;
+        }
+    }
+
+})();
+
+const instanceA = singleTon.getInstance();
+console.log(instanceA);
+
+const instanceB = singleTon.getInstance();
+console.log(instanceB);
+
+console.log('COMPARE', instanceA === instanceB);
